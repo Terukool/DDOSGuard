9d67805cce77048f26b123b20d60844b7b229f8e
Terukool <taabkl@gmail.com>
2024-06-01 14:27:12 +0300

    CR

--- DDOSSimulator/RequestSimulator/Logic/ConsoleIO.cs
+++ DDOSSimulator/RequestSimulator/Logic/ConsoleIO.cs
@@ -1,9 +1,4 @@
 ﻿using RequestSimulatorClient.Logic.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RequestSimulatorClient.Logic
 {
--- DDOSSimulator/RequestSimulator/Program.cs
+++ DDOSSimulator/RequestSimulator/Program.cs
@@ -1,14 +1,22 @@
 ﻿using RequestSimulatorClient.Logic;
 
 const string SERVER_URL = "http://localhost:4293";
-Tuple<int, int> waitRangeMs = new(100, 1500);
+const int MIN_WAIT_MS = 100;
+const int MAX_WAIT_MS = 1500;
 
+var waitRangeMs = new Tuple<int, int>(MIN_WAIT_MS, MAX_WAIT_MS);
 var cancellationTokenSource = new CancellationTokenSource();
-ConsoleIO io = new();
+var io = new ConsoleIO();
 
 io.Write("Enter the amount of HTTP clients to simulate:");
 string numOfClientsInput = io.Read();
-int numOfClients = int.Parse(numOfClientsInput);
+int.TryParse(numOfClientsInput, out int numOfClients);
+
+if (numOfClients <= 0)
+{
+    io.Write("The number of clients must be greater than 0.");
+    return;
+}
 
 var simulation = new Simulation(io, numOfClients, waitRangeMs, SERVER_URL);
 simulation.Simulate(cancellationTokenSource.Token);
